3881f9c33ed7d8b4a711b8b1dab895d1f9c55f7b
Russel Harvey Mercado IV <76506387+Muxxano@users.noreply.github.com>
2022-10-03 20:36:36 +0800

    sales_record-done

--- StockInventory/registration/urls.py
+++ StockInventory/registration/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('login/', views.User_login.as_view(), name = 'login'),
     path('register_c/', views.RegisterCustomer.as_view(), name = 'register_customer'),
     path('register_e/', views.RegisterEmployee.as_view(), name = 'register_employee'),
-    path('supplier',views.Supplier.as_view(), name = 'supplier'),
+    path('supplier/',views.Supplier.as_view(), name = 'supplier'),
+    path('salesrecord/', views.SalesRecord.as_view(), name='sales_record'),
     # path('product',views.Product.as_view(), name = 'product'),
 
 ]
--- StockInventory/registration/views.py
+++ StockInventory/registration/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
-from registration.forms import RegisterCustomerForm, RegisterEmployeeForm, SupplierForm
+from .forms import *
 from registration.models import *
 
 
@@ -11,7 +11,7 @@ from registration.models import *
 def index(request):
     return render(request, 'registration/index.html')
 def user_home(request):
-    return render(request, 'registration/user_home.html')
+    return render(request, 'registration/user_home.html', {'user_name':request.session['username']})
 
 class RegisterCustomer(View):
     template = 'registration/register_customer.html'
@@ -63,15 +63,39 @@ class User_login(View):
                 request.session['employee_id'] = user.user_ID
                 request.session['username'] = user.username
                 request.session['type'] = user.type
-                return redirect(reverse('user_home'))
+                return render(request,'registration/user_home.html', {'user_name': user.username})
         except User.DoesNotExist:
             user = None
 
         return render(request, self.template,{'msg':'Incorrect username/ password.'})
 
 
+class SalesRecord(View):
+    template = 'registration/sales_record.html'
+    form = SalesForm()
+    records_list = Sales.objects.order_by('salesID')
+    def get(self,request):
+        return render(request, self.template, {'form': self.form,
+                                               'records' : self.records_list,
+                                               'user_name':request.session['username']})
+    def post(self, request):
+        self.form = SalesForm(request.POST)
+
+        if self.form.is_valid():
+            self.form.save()
+            return redirect(reverse('registration:sales_record'))
+        else:
+            print("Unsuccesful")
+        return redirect(reverse('user_home'))
+
+
+
+
+
+
+
 class Supplier(View):
-    template = 'supplier'
+    template = 'registration/supplier.html'
     form = SupplierForm()
 
     def get(self,request):
