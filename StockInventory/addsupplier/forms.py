a860526515ee4904199e3aaef6a5b49e3587dc8e
JustPi <pamela.canedo@cit.edu>
2022-10-04 00:15:47 +0800

    forms gibalik

---
+++ StockInventory/addsupplier/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+
+from StockInventory.registration.models import Supplier
+
+
+class SupplierForm(ModelForm):
+    # companyName = forms.CharField(widget=forms.TextInput())
+    # address = forms.CharField(widget=forms.TextInput())
+    # contact = forms.CharField(widget=forms.TextInput())
+
+    class Meta:
+        model = Supplier
+        fields = '__all__'
