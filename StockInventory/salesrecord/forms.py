554a2bc8199c8fecc05c0ed64ede00e64e9e7730
Russel Harvey Mercado IV <76506387+Muxxano@users.noreply.github.com>
2022-10-16 10:19:39 +0800

    re_design and fixes

--- StockInventory/salesrecord/forms.py
+++ StockInventory/salesrecord/forms.py
@@ -2,8 +2,13 @@ from django.forms import ModelForm, NumberInput
 from django import forms
 from registration.models import Sales
 
+from registration.models import Customer, Employee, Product
+
 
 class SalesForm(ModelForm):
+    cUser_ID = forms.ModelChoiceField(queryset = Customer.objects.only('user_ID'),label = "Customer")
+    eUser_ID = forms.ModelChoiceField(queryset = Employee.objects.only('user_ID'),label = "Employee")
+    product_ID = forms.ModelChoiceField(queryset = Product.objects.only('product_ID'),label = "Product")
     dateOfSale = forms.DateField(widget=NumberInput(attrs={'type': 'date'}))
     class Meta:
         model = Sales
