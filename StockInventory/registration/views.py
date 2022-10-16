92f33dd23d3bb023c350be8e36d69cfbce867f65
Russel Harvey Mercado IV <76506387+Muxxano@users.noreply.github.com>
2022-10-16 20:06:36 +0800

    re

--- StockInventory/registration/views.py
+++ StockInventory/registration/views.py
@@ -1,3 +1,4 @@
+from django.db import connection
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
@@ -12,16 +13,52 @@ def index(request):
     request.session['username'] = None
     return render(request, 'registration/index.html')
 
+
 class User_home(View):
+    template = 'registration/user_home.html'
+
     def get(self, request):
         records_list = Sales.objects.order_by('dateOfSale')
-        trans_list = Transactions.objects.order_by('salesCount')
+
+        cursor = connection.cursor()
 
         if request.session['username'] == None:
             return render(request, 'registration/index.html')
-        return render(request, 'registration/user_home.html', {'user_name':request.session['username'],
-                                                               'records': records_list,
-                                                               'transaction':trans_list})
+
+        cursor.callproc('dbstockinventory.sales_transaction', [request.session['FDATE'],
+                                                               request.session['TDATE']])
+        trans = list(cursor.fetchall())
+        cursor.close()
+        return render(request, self.template, {'user_name': request.session['username'],
+                                               'records': records_list,
+                                               'trans': trans})
+
+    def post(self, request):
+
+        request.session['FDATE'] = request.POST['fromDate']
+        request.session['TDATE'] = request.POST['toDate']
+
+        cursor = connection.cursor()
+        cursor.callproc('dbstockinventory.sales_transaction', [request.session['FDATE'],
+                                                               request.session['TDATE']])
+        trans = cursor.fetchall()
+        for t in trans:
+            prd = Product.objects.get(prodName=t[2])
+            try:
+                trans_record = Transactions.objects.get(product=prd)
+                if t[0] > trans_record.salesCount:
+                    update_record = Transactions(transactionID=trans_record.transactionID,
+                                                 salesCount=t[0],
+                                                 supplier=trans_record.supplier,
+                                                 product=trans_record.product)
+                    update_record.save()
+
+            except:
+                Transactions.objects.create(salesCount=t[0],
+                                            supplier=Supplier.objects.get(companyName=t[1]),
+                                            product=prd)
+        cursor.close()
+        return redirect(reverse('user_home'))
 
 
 def user_logout(request):
@@ -34,7 +71,7 @@ class RegisterCustomer(View):
     form = RegisterCustomerForm()
 
     def get(self, request):
-        return render(request, self.template, {'form':self.form})
+        return render(request, self.template, {'form': self.form})
 
     def post(self, request):
         self.form = RegisterCustomerForm(request.POST)
@@ -44,13 +81,14 @@ class RegisterCustomer(View):
             print("unsucessful")
         return index(request)
 
+
 class RegisterEmployee(View):
     template = 'registration/register_employee.html'
     form = RegisterEmployeeForm()
     registered = False
 
     def get(self, request):
-        return render(request, self.template, {'form':self.form})
+        return render(request, self.template, {'form': self.form})
 
     def post(self, request):
         self.form = RegisterEmployeeForm(request.POST)
@@ -61,6 +99,7 @@ class RegisterEmployee(View):
             print("unsucessful")
         return render(request, self.template, {'form': self.form,
                                                'registered': self.registered})
+
 
 class User_login(View):
     template = 'registration/login.html'
@@ -73,27 +112,27 @@ class User_login(View):
         password = request.POST['password']
 
         try:
-            user = User.objects.get(username = username)
+            user = User.objects.get(username=username)
             if user.password == password:
                 request.session['username'] = user.username
                 request.session['employee_id'] = user.user_ID
                 return redirect(reverse('user_home'))
         except User.DoesNotExist:
             user = None
-        return render(request, self.template,{'msg':'Incorrect username/ password.'})
+        return render(request, self.template, {'msg': 'Incorrect username/ password.'})
 
 
 class EditProfile(View):
     template = 'registration/editProfile.html'
 
-    def get(self,request):
+    def get(self, request):
         employee = Employee.objects.get(pk=request.session['employee_id'])
         form = RegisterEmployeeForm(instance=employee)
-        return render(request,self.template,{'form':form})
+        return render(request, self.template, {'form': form})
 
-    def post(self,request):
+    def post(self, request):
         employee = Employee.objects.get(pk=request.session['employee_id'])
-        form = RegisterEmployeeForm(request.POST,instance= employee)
+        form = RegisterEmployeeForm(request.POST, instance=employee)
 
         if form.is_valid():
             form.save()
@@ -104,7 +143,7 @@ class EditProfile(View):
 
 
 
-    
+
 
 
 
