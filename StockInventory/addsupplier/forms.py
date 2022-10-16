c21f32edabf0437c9dd2916dfb3a9db5c9aca5e0
JustPi <pamela.canedo@cit.edu>
2022-10-16 21:04:41 +0800

    try

--- StockInventory/addsupplier/forms.py
+++ StockInventory/addsupplier/forms.py
@@ -6,13 +6,11 @@ from registration.validators import checkNumber
 
 
 class SupplierForm(ModelForm):
-    # companyName = forms.CharField(widget=forms.TextInput())
-    # address = forms.CharField(widget=forms.TextInput())
     mobileNumber = forms.CharField(validators=[checkNumber],
                                    max_length = 13)
 
     def __init__(self, *args, **kwargs):
-        super(SupplierForm, self).__init__(*args,**kwargs)
+        super(SupplierForm, self).__init__(*args, **kwargs)
         self.fields['companyName'].label = 'Company Name'
         self.fields['mobileNumber'].label = 'Mobile Number'
 
