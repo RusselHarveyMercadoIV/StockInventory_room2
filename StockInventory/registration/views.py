e8fa13e761d8d5ba7acd2435131d4a652fa91b5c
Russel Harvey Mercado IV <76506387+Muxxano@users.noreply.github.com>
2022-10-16 22:20:56 +0800

    re

--- StockInventory/registration/views.py
+++ StockInventory/registration/views.py
@@ -18,6 +18,7 @@ class User_home(View):
     template = 'registration/user_home.html'
 
     def get(self, request):
+        trans = False
         records_list = Sales.objects.order_by('dateOfSale')
 
         if request.session['username'] == None:
